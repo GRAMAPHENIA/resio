82a855eae42db18f6ecb209a770ef56a97c21749
gramaphenia <dicoratojuanpablo@gmail.com>
2025-10-17 05:30:42 -0300

    feat: iniciar commit

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home(){
+  return (
+    <main>
+      <h1>RESIO</h1>
+      <p>Explora una amplia variedad de opciones de alojamiento que se adapten a tus necesidades.</p>
+    </main>
+  )
+}
