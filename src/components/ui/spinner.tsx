451043b56ed70e8b6e68ee1b9a32420875c1c91d
gramaphenia <dicoratojuanpablo@gmail.com>
2025-10-27 21:08:20 -0300

    feat: meoras

--- src/components/ui/spinner.tsx
+++ src/components/ui/spinner.tsx
@@ -5,7 +5,7 @@ interface SpinnerProps {
 
 export default function Spinner({ size = 'md', className = '' }: SpinnerProps) {
   const sizeClasses = {
-    sm: 'w-4 h-4 border',
+    sm: 'w-4 h-4 border', 
     md: 'w-8 h-8 border-2',
     lg: 'w-12 h-12 border-2'
   }
